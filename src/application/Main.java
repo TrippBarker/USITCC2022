3b58afef922c93dc3f2fb1684837654eeb4a339a
Tripp Barker <colemanbarker@gmail.com>
2023-03-26 14:42:51 -0400

    working on scenes

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ import javafx.scene.Scene;
 public class Main extends Application {
 	
 	public static final String TABLE_VIEW_SCENE_PATH = "scenes/TableView.fxml";
-	public static final String LOGIN_SCENE_PATH = "scenes/LoginScene.fxml";
+	public static final String LOGIN_SCENE_PATH = "scenes/CreateNewUser.fxml";
 	
 	public static final GrandSlamWriter gSlamWriter = new GrandSlamWriter();
 	public static final GrandSlamReader gSlamReader = new GrandSlamReader();
